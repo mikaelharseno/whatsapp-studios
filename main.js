5026709d6e827b314f5603b10c178c7a83f673ce
mikaelharseno <mikael_harseno@yahoo.com>
2024-04-14 18:06:16 +0700

    Starting commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+// Create a new client instance
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        executablePath: '/usr/bin/google-chrome-stable',
+    }
+});
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Client is ready!');
+});
+
+// When the client received QR-Code
+client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
+});
+
+// Start your client
+client.initialize();
