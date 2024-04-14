84ffaf0f1acfa441e72b9e859d7df1083bdb9cb7
mikaelharseno <mikael_harseno@yahoo.com>
2024-04-14 18:21:32 +0700

    Added express

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const express = require('express');
+const port = 3000;
 
 // Create a new client instance
 const client = new Client({
@@ -21,3 +23,43 @@ client.on('qr', (qr) => {
 
 // Start your client
 client.initialize();
+
+const app = express();
+app.use(express.json()) // for parsing application/json
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+
+app.post('/send-message', (req, res) => {
+    console.log(req.body)
+    res.send('hello world');
+})
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+// async function sendNewMessageWithTimeout(customerNumber, message, timeout = 3000) {
+//   var paramsMain = { 
+//     "chatId": customerNumber + "@c.us",
+//     "contentType": "string",
+//     "content": message
+//   };
+// }
+
+// async function sendSheepGif(customerNumber) {
+//   var paramsMain = { 
+//     "chatId": customerNumber + "@c.us",
+//     "contentType": "MessageMediaGif",
+//     "content": "Send GIF"
+//   };
+// }
+
+// async function sendImage(customerNumber, imageUrl, timeout = 3000) {
+//   var paramsMain = { 
+//     "chatId": customerNumber + "@c.us",
+//     "contentType": "MessageMediaFromURL",
+//     "content": imageUrl,
+//   };
+// }
